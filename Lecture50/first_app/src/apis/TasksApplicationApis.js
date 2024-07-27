9057dbfb79cde300bdd7a32aac26179c94410984
Debasis <sahurocky524@gmail.com>
2024-07-27 09:24:42 +0530

    updates

--- Lecture50/first_app/src/apis/TasksApplicationApis.js
+++ Lecture50/first_app/src/apis/TasksApplicationApis.js
@@ -1,15 +1,19 @@
 import axios from "axios";
+import { getUserToken } from "../utils/LoginUtils";
 
 export const TaskApplicationBackend = axios.create({
     baseURL : 'https://localhost:3000',
 })
 
-// TaskApplicationBackend.interceptors.request.use(
-//     (config)=>{
-//         console.log("Run this configuration");
-//         console.log(config);
-//     },
-//     (error)=>{
-//         return Promise.reject(error);
-//     }
-// );
+TaskApplicationBackend.interceptors.request.use(
+    (config)=>{
+        const token = getUserToken();
+        if(token){
+            config.headers = {Authorization: token};
+            console.log("Inside interceptor");
+        }
+    },
+    (error)=>{
+        return Promise.reject(error);
+    }
+);
