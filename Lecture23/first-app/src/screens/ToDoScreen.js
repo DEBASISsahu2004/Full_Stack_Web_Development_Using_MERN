07691203dee01099cb2be293db0f440a3fc6e4b7
Debasis <sahurocky524@gmail.com>
2024-09-10 16:50:09 +0530

    updates

--- Lecture23/first-app/src/screens/ToDoScreen.js
+++ Lecture23/first-app/src/screens/ToDoScreen.js
@@ -1,39 +1,46 @@
+// import { Component } from "react";
 import { useState } from "react";
-import Task from "../components/Task.js"
-import AddTask from "../components/AddTask.js";
+import Task from "../components/Task";
+import AddTask from "../components/AddTask";
 
-const ToDoScreen = function(){
+const ToDoScreen = () => {
     const [taskList, setTaskList] = useState([]);
 
-    let addNewTask = (task)=>{
-        setTaskList([...taskList, {...task, createdDate: new Date()}])
-    }
-    
     return (
         <>
-        <div className="screen">
-            <h1 className="ui heading center">To Do List</h1>
-            <div>
-                <button onClick={(e)=>{
-                    setTaskList([...taskList, {
-                        title: "Go To Gym",
-                        description: "Going to gym is good for muscle growth.",
-                        createdDate: new Date(),
-                    }])
-                }} className="ui secondary button">Add Task</button>
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div>
+                    <button
+                        onClick={(e) => {
+                            setTaskList([
+                                ...taskList,
+                                {
+                                    title: "Go to gym",
+                                    description:
+                                        "Going to gym is good for muscle growth.",
+                                    createdDate: new Date(),
+                                },
+                            ]);
+                        }}
+                        className="ui secondary button"
+                    >
+                        Add Task
+                    </button>
 
-                <section>
-                    <div className="ui cards">
-                        {taskList.map((task, index) => (
-                            <Task task={task} key={index}/>
-                        ))}
-                    </div>
-                </section>
+                    <section>
+                        <div class="ui cards">
+                            {taskList.map((task, index) => (
+                                <Task task={task} key={index} />
+                            ))}
+                        </div>
+                    </section>
+                </div>
+
+                <AddTask />
             </div>
-        </div>
-        <AddTask />
         </>
     );
-}
+};
 
 export default ToDoScreen;
